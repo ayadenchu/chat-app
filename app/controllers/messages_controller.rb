b0a6d3e0da2d90bf722b2c98f42894d06fc925a8
ayadenchu <saion.denchu316@gmail.com>
2020-10-15 18:11:38 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,7 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
   end
 
   def create
@@ -10,12 +11,14 @@ class MessagesController < ApplicationController
     if @message.save
       redirect_to room_messages_path(@room)
     else
+      @message = @room.message.includes(:user)
       render :index
-    end
+    end  
   end
   private
 
  def message_params
    params.require(:message).permit(:content).merge(user_id: current_user.id)
  end
+
 end
